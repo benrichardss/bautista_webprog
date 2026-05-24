255166124b25cc0b1ce53af5ac19eb642502a88c
Aron <aronbautista61@gmail.com>
2026-05-24 21:27:53 +0800

    production

--- bautista-server/index.js
+++ bautista-server/index.js
@@ -52,4 +52,5 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: "Server Error" });
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
